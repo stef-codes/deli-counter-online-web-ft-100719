b868bab131f20859527e865f740ba0472e28345f
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 02:03:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,11 @@
 katz_deli = []
 
 def line(people)
-customers = []
+    customers = []
+    counter = 0
     people.each_with_index do |people, number| 
       customers << " #{number + 1}. #{people}"
+    counter += 1
     end 
     return "The line is currently: #{customers.join("")}"
 end
