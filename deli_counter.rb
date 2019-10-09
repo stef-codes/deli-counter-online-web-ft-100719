deabc1ce94f4c498385154b1fd040eeaa80b399e
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 01:23:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,8 @@ katz_deli = []
 def line(people)
   if people.length == 0
     puts "The line is currently empty."
+  else people.each_with_index do |people, number| 
+      puts 
+    end 
   end
 end
