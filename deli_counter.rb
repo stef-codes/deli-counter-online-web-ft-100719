89c7ffe7e7af8416376c0e7509b7991bc057ed8f
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 02:10:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,11 @@ def line(people)
     customers = []
     counter = 0
     people.each_with_index do |people, number| 
-      customers << " #{number + 1}. #{people}"
+      customers << "#{number + 1}. #{people}"
     counter += 1
   end 
     puts "The line is currently: #{customers.join("")}"
   else 
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   end
 end 
