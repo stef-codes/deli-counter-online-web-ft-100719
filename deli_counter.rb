6426daa456a6e55ef8822cf08133f0b31d3e9e83
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 02:00:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,6 @@
 katz_deli = []
 
 def line(people)
-#  if people.length == 0
-#    puts "The line is currently empty."
-#  else 
 customers = []
     people.each_with_index do |people, number| 
       customers << " #{number + 1}. #{people}"
