47dc7cbb02d83cfde400d4062f7c4efb72661811
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 02:08:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
 katz_deli = []
 
 def line(people)
+  if people.length >= 1 
     customers = []
     counter = 0
     people.each_with_index do |people, number| 
       customers << " #{number + 1}. #{people}"
     counter += 1
-    end 
-    return "The line is currently: #{customers.join("")}"
-end
+  end 
+    puts "The line is currently: #{customers.join("")}"
+  else 
+    puts "The line is currently empty"
+  end
+end 
