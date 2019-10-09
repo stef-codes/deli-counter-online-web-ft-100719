99c27cb3b831e8a6bcd1d67cef6affad41ebaa46
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 02:30:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,13 @@ def line(people)
 end 
 
 def take_a_number(line,last_person)
-    line.push(last_person)
+    if line.length == 0 
+      line << last_person
+    elsif line.length > 0
+    
+    else 
+      line.push(last_person)
+    end
 #  puts "Welcome, #{last_person}. You are number #{line.length} in line."
 end 
   
