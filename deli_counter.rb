96b360ffb524a272bbcaaaef773fb2bdd9f41004
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 02:01:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,5 @@ customers = []
     people.each_with_index do |people, number| 
       customers << " #{number + 1}. #{people}"
     end 
-    puts "The line is currently: #{customers.join("")}"
+    return "The line is currently: #{customers.join("")}"
 end
