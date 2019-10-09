af37f6fca53e6ab7af6ed31c5e5f8d0122a8e29a
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 01:26:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 katz_deli = []
 
 def line(people)
-  if people.length == 0
-    puts "The line is currently empty."
-  else people.each_with_index do |people, number| 
+#  if people.length == 0
+#    puts "The line is currently empty."
+#  else 
+    people.each_with_index do |people, number| 
       puts "The line is currently: #{number + 1} #{people}""
   end
 end
