aa52942f1373c320d1eadafd226440759d59db13
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-09 01:25:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,5 @@ def line(people)
     puts "The line is currently empty."
   else people.each_with_index do |people, number| 
       puts "The line is currently: #{number + 1} #{people}""
-    end 
   end
 end
